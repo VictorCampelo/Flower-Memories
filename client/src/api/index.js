012c02fc537bc9b6b849a626576292f81aa79e21
Victor Campelo <victor_campelo@outlook.com>
2021-06-09 22:16:52 -0300

    feat: access application api on local network

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'http://192.168.0.6:5000' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
